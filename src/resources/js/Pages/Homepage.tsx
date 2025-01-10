e1e92bdc9304a2ebffcbd9e66d4e7110978cd9b8
Seth Teichman <smt5541@rit.edu>
2025-01-10 12:50:19 -0500

    Remove unused props from Homepage, rename component and update APP_NAME

---
+++ src/resources/js/Pages/Homepage.tsx
@@ -0,0 +1,17 @@
+import { Head } from '@inertiajs/react';
+import MainLayout from "@/Layouts/MainLayout";
+import {Container, Text, Title} from "@mantine/core";
+
+export default function Homepage() {
+    return (
+        <MainLayout>
+            <Head title="RIT ArtistConnect" />
+            <Container mt={"lg"}>
+                <Title order={1}>Welcome to RIT ArtistConnect!</Title>
+                <Text>
+                    ArtistConnect is an unofficial resource to connect RIT artists who are accepting commissions with interested parties in the RIT community.
+                </Text>
+            </Container>
+        </MainLayout>
+    );
+}
