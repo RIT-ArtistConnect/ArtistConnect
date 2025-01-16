332b454067aa31d2d9626c1fadfb09d9505537c4
Seth Teichman <smt5541@rit.edu>
2025-01-15 21:46:19 -0500

    Make admin homepage grid responsive

--- src/resources/js/Pages/Admin/AdminHome.tsx
+++ src/resources/js/Pages/Admin/AdminHome.tsx
@@ -1,21 +1,19 @@
 import MainLayout from "@/Layouts/MainLayout";
-import {Card, Grid, Stack, Title} from "@mantine/core";
+import {Card, SimpleGrid, Stack, Title} from "@mantine/core";
 import {Link} from "@inertiajs/react";
 import {IconUsers} from "@tabler/icons-react";
 
 export default function AdminHome({userCount}: {userCount: number}) {
     return <MainLayout title={"Admin Homepage"}>
-        <Grid>
-            <Grid.Col span={4}>
-                <Link href={route("admin.users")}>
-                    <Card bg={"ac-blue"}>
-                        <Stack gap={"md"}>
-                            <IconUsers/>
-                            <Title order={3}>{userCount} Users</Title>
-                        </Stack>
-                    </Card>
-                </Link>
-            </Grid.Col>
-        </Grid>
+        <SimpleGrid cols={{base: 1, xs: 2, sm: 3, md: 3, lg: 4}}>
+            <Link href={route("admin.users")}>
+                <Card bg={"ac-blue"}>
+                    <Stack gap={"md"}>
+                        <IconUsers/>
+                        <Title order={3}>{userCount} Users</Title>
+                    </Stack>
+                </Card>
+            </Link>
+        </SimpleGrid>
     </MainLayout>
 }
