7a733235ece7c1dd7a996f4815575c225c0c4215
ajh6754 <ajh6754@g.rit.edu>
2025-01-25 23:51:19 -0500

    successfully created tables

--- src/database/migrations/2025_01_26_034731_create_tag_table.php
+++ src/database/migrations/2025_01_26_034731_create_tag_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tag', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->id();
         });
     }
@@ -21,6 +21,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tag');
+        Schema::dropIfExists('tags');
     }
 };
