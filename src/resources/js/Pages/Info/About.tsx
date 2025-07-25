2f64ea547301cbc14941f3eb59dde3d23412a19e
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2025-07-25 19:35:15 -0400

    Move code to the actual About page

--- src/resources/js/Pages/Info/About.tsx
+++ src/resources/js/Pages/Info/About.tsx
@@ -1,46 +1,62 @@
 import MainLayout from '@/Layouts/MainLayout';
-import { Container, Stack, Text, Title } from '@mantine/core';
+import InfoSection from './Partials/InfoSection';
+
+import { Box, Grid, Stack, Text } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 
 export default function Homepage() {
-    const sectionHeaderGap = 2;
+    const isMobile = !useMediaQuery('(min-width: 45em)');
+    console.log(isMobile);
 
     return (
-        <MainLayout title={"About"}>
-                <Stack gap={40}>
-                    <Stack gap={1}>
-                        <Text>
-                            RIT ArtistConnect is an unofficial database of
-                            creatives at RIT. This website seeks to connect
-                            students, faculty, and alumni with artists to help
-                            with personal, academic, and professional artistic
-                            needs. Any member of the community with an active
-                            RIT email address can join for free, and users who
-                            fill out our submission form at the bottom of this
-                            page.
-                        </Text>
+        <MainLayout>
+            <Grid columns={4}>
+                <Grid.Col span={isMobile ? 4 : 1}>
+                    <Box bg="red.5" p="40" h="100%" w="100%"></Box>
+                </Grid.Col>
+                <Grid.Col span={isMobile ? 4 : 3}>
+                    <Stack gap={30}>
+                        <InfoSection title={'About'} order={1}>
+                            <Text>
+                                RIT ArtistConnect is an unofficial database of
+                                creatives at RIT. This website seeks to connect
+                                students, faculty, and alumni with artists to
+                                help with personal, academic, and professional
+                                artistic needs. Any member of the community with
+                                an active RIT email address can join for free,
+                                and users who fill out our submission form at
+                                the bottom of this page.
+                            </Text>
+                        </InfoSection>
+                        <InfoSection
+                            title={'ArtistConnect for clients'}
+                            order={2}
+                        >
+                            <Text>
+                                When you sign up for ArtistConnect, you can
+                                search through the database and contact artists
+                                of various disciplines.
+                            </Text>
+                        </InfoSection>
+                        <InfoSection
+                            title={'ArtistConnect for artists'}
+                            order={2}
+                        >
+                            <Text>
+                                With an account, you can create up to three
+                                connected artist profiles to promote your
+                                artwork. Artist Profiles include information
+                                including: a short bio about you and your work,
+                                up to four featured pieces, links to external
+                                social media accounts, and a customizable
+                                contact button on your profile. Artist Profiles
+                                are free to create, though they require
+                                additional moderator verification.
+                            </Text>
+                        </InfoSection>
                     </Stack>
-                    <Stack gap={sectionHeaderGap}>
-                        <Title order={2}>ArtistConnect for clients</Title>
-                        <Text>
-                            When you sign up for ArtistConnect, you can search
-                            through the database and contact artists of various
-                            disciplines.{' '}
-                        </Text>
-                    </Stack>
-                    <Stack gap={sectionHeaderGap}>
-                        <Title order={2}>ArtistConnect for artists</Title>
-                        <Text>
-                            With an account, you can create up to three
-                            connected artist profiles to promote your artwork.
-                            Artist Profiles include information including: a
-                            short bio about you and your work, up to four
-                            featured pieces, links to external social media
-                            accounts, and a customizable contact button on your
-                            profile. Artist Profiles are free to create, though
-                            they require additional moderator verification.
-                        </Text>
-                    </Stack>
-                </Stack>
+                </Grid.Col>
+            </Grid>
         </MainLayout>
     );
 }
