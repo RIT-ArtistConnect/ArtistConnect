c43038a9724d880f37117b88dae61ee9e5f3266a
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2025-07-31 21:32:45 -0400

    Add new body text

--- src/resources/js/Pages/Info/About.tsx
+++ src/resources/js/Pages/Info/About.tsx
@@ -11,8 +11,14 @@ export default function Homepage() {
     return (
         <MainLayout>
             <Grid columns={4}>
-                <Grid.Col span={"auto"}>
-                    <Box bg="red.5" p="40" h="100%" w={isMobile ? "20em" : "10em"} m="auto"></Box>
+                <Grid.Col span={'auto'}>
+                    <Box
+                        bg="red.5"
+                        p="40"
+                        h="100%"
+                        w={isMobile ? '20em' : '10em'}
+                        m="auto"
+                    ></Box>
                 </Grid.Col>
                 <Grid.Col span={isMobile ? 4 : 3}>
                     <Stack gap={30}>
@@ -23,35 +29,64 @@ export default function Homepage() {
                                 students, faculty, and alumni with artists to
                                 help with personal, academic, and professional
                                 artistic needs. Any member of the community with
-                                an active RIT email address can join for free,
-                                and users who fill out our submission form at
-                                the bottom of this page.
+                                an active RIT email address can join for free.
+                            </Text>
+                            <Text>
+                                This project is created and managed by RIT
+                                Drawing Club. For more information on Drawing
+                                Club, please visit our CampusGroups page,
+                                website, or Discord server.
+                            </Text>
+
+                            <Text>
+                                On this website, we separate artist-client
+                                connections into three different categories:
                             </Text>
                         </InfoSection>
-                        <InfoSection
-                            title={'ArtistConnect for clients'}
-                            order={2}
-                        >
+                        <InfoSection title={'Mentorship'} order={2}>
                             <Text>
-                                When you sign up for ArtistConnect, you can
-                                search through the database and contact artists
-                                of various disciplines.
+                                Underclassmen or hobbyist artists seeking
+                                feedback or other forms of guidance from more
+                                experienced creatives within their desired
+                                field.
                             </Text>
                         </InfoSection>
-                        <InfoSection
-                            title={'ArtistConnect for artists'}
-                            order={2}
-                        >
+                        <InfoSection title={'Teamwork'} order={2}>
                             <Text>
-                                With an account, you can create up to three
-                                connected artist profiles to promote your
-                                artwork. Artist Profiles include information
-                                including: a short bio about you and your work,
-                                up to four featured pieces, links to external
-                                social media accounts, and a customizable
-                                contact button on your profile. Artist Profiles
-                                are free to create, though they require
-                                additional moderator verification.
+                                Ideal for student teams seeking other students
+                                to assist them with class projects, such as
+                                finding additional background artists or rough
+                                animators for animation thesis films, designers
+                                for game assets, and so on. This option comes
+                                with the expectation that a project is unpaid or
+                                for academics.
+                            </Text>
+                        </InfoSection>
+                        <InfoSection title={'Freelance Work'} order={2}>
+                            <Text>
+                                The most common connection, this covers anyone
+                                offering art for money. Commissioning logos for
+                                a business, illustrations for personal use or
+                                gifts, or any other paid services fits under
+                                this filter.
+                            </Text>
+                            <Text>
+                                While users can browse the discover page without
+                                signing in, contact information and links for
+                                artists are obscured for privacy reasons.
+                            </Text>
+                        </InfoSection>
+                        <InfoSection title={'Client Accounts'} order={2}>
+                            <Text>
+                                When you sign up for ArtistConnect and confirm
+                                your RIT email address, you will have full
+                                access to the database to find artists open for
+                                a variety of services.
+                            </Text>
+                        </InfoSection>
+                        <InfoSection title={'Artist Profiles'} order={2}>
+                            <Text>
+                                With an account, you can create up to three connected artist profiles to promote your artwork. With these profiles, you can showcase what services you offer, link to a portfolio website and a contact email. Artist profiles are manually verified to ensure that none of the work featured violates RIT media policies. 
                             </Text>
                         </InfoSection>
                     </Stack>
