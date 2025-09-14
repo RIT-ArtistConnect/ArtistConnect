ba0d7de41248639ebe56109c0280893abe9f7385
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2025-09-14 19:43:58 -0400

    Remove debug print

--- src/resources/js/Pages/Info/Resources.tsx
+++ src/resources/js/Pages/Info/Resources.tsx
@@ -6,7 +6,6 @@ import { useMediaQuery } from '@mantine/hooks';
 
 export default function Resources() {
     const isMobile = !useMediaQuery('(min-width: 45em)');
-    console.log(isMobile);
 
     return (
         <MainLayout>
