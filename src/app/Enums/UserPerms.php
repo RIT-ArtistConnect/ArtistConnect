aca97d93b9d64b54f4190a8dcab8d8726f6e9264
jw5250 <jw5250@rit.edu>
2025-11-23 12:29:46 -0500

    first implementation version of user permissions

---
+++ src/app/Enums/UserPerms.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Enums;
+
+enum userPerms: int
+{
+    case nonexistant = -1;
+    case unverified = 0;
+    case verified = 1;
+    case mod = 2;
+    case admin = 3;
+    case headAdmin = 4;
+    case siteOwner = 5;
+
+    /**
+     * Assumes that the permissions associated with a userPerms value include that of all values less than it 
+     * (outside of permissions exclusive to it). If true, user has access. If false, user doesn't.
+     * @var bool
+     */
+    public function canAccess(userPerms $permission, userPerms $requirement): bool {
+        return $permission >= $requirement;
+    }
+}
